717bb03045c8891dce0e3ce658c240a33dab8ea4
dksalsgus <alsgus2ek@naver.com>
2021-10-06 22:46:31 +0900

    base Entity type update

--- src/cls/baseentity.ts
+++ src/cls/baseentity.ts
@@ -1,8 +1,8 @@
 import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 export class BaseEntity {
-  @CreateDateColumn({ nullable: true })
+  @CreateDateColumn({ nullable: true, type: 'datetime' })
   createdAt: Date;
-  @UpdateDateColumn({ nullable: true })
+  @UpdateDateColumn({ nullable: true, type: 'datetime' })
   updatedAt: Date;
 }
